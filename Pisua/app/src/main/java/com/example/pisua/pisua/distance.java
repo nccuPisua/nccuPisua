8fe2fc4a92bc8d527e7c43aa04f39f8f0dc6dcca
teresa5605 <teresa5605@gmail.com>
2015-08-03 21:43:17 +0800

    distance test

---
+++ Pisua/app/src/main/java/com/example/pisua/pisua/distance.java
@@ -0,0 +1,103 @@
+package com.example.pisua.pisua;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+ @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi,
+                final byte[] scanRecord) {
+            int startByte = 2;
+            boolean patternFound = false;
+            // 寻找ibeacon
+            // 先依序尋找第2到第8陣列的元素
+            while (startByte <= 5) {
+                if (((int) scanRecord[startByte + 2] & 0xff) == 0x02 && 
+                        // Identifies an iBeacon
+                        ((int) scanRecord[startByte + 3] & 0xff) == 0x15) { 
+                            // Identifies correct data length
+                    patternFound = true;
+                    break;
+                }
+                startByte++;
+            }
+            
+            // 如果找到了的话
+            if (patternFound) {
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);  
+                // 转换为16进制
+                byte[] uuidBytes = new byte[16];
+                // 來源、起始位置
+                System.arraycopy(scanRecord, startByte + 4, uuidBytes, 0, 16);
+                String hexString = bytesToHex(uuidBytes);
+
+                // ibeacon的UUID值
+                String uuid = hexString.substring(0, 8) + "-"
+                        + hexString.substring(8, 12) + "-"
+                        + hexString.substring(12, 16) + "-"
+                        + hexString.substring(16, 20) + "-"
+                        + hexString.substring(20, 32);
+
+                // ibeacon的Major值
+                int major = (scanRecord[startByte + 20] & 0xff) * 0x100
+                        + (scanRecord[startByte + 21] & 0xff);
+
+                // ibeacon的Minor值
+                int minor = (scanRecord[startByte + 22] & 0xff) * 0x100
+                        + (scanRecord[startByte + 23] & 0xff);
+
+                String ibeaconName = device.getName();
+                String mac = device.getAddress();
+                int txPower = (scanRecord[startByte + 24]);
+                double distance = calculateAccuracy(txPower,rssi);
+                Log.d(Tag,bytesToHex(scanRecord));
+                Log.d(Tag, "Name：" + ibeaconName + "\nMac：" + mac
+                        + " \nUUID：" + uuid + "\nMajor：" + major + "\nMinor："
+                        + minor + "\nTxPower：" + txPower + "\nrssi：" + rssi);
+
+                Log.d(Tag,"distance："+calculateAccuracy(txPower,rssi));
+                /*
+                Intent localIntent = new Intent(BROADCAST_ACTION)
+                        // Puts the status into the Intent
+                        .putExtra(EXTENDED_DATA_STATUS, calculateAccuracy(txPower,rssi));
+                // Broadcasts the Intent to receivers in this app.
+                sendBroadcast(localIntent);
+                 */
+                Intent intent = new Intent();
+                intent.setAction("i_am_hungry");
+                intent.putExtra("HELLO", distance);
+                sendBroadcast(intent);
+
+            }
+        }
+		static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    // 字組轉16進制
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    // 計算距離
+    protected static double calculateAccuracy(int txPower, double rssi) {
+        if (rssi == 0) {
+            return -1.0; // if we cannot determine accuracy, return -1.
+        }
+
+        double ratio = rssi * 1.0 / txPower;
+        if (ratio < 1.0) {
+            return Math.pow(ratio, 10);
+        } else {
+            double accuracy = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
+            return accuracy;
+        }
+    }
